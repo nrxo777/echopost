f4480215d730af3f5c4eba14f5b7856cd55c2e31
nrxo777 <noorxcalma@gmail.com>
2025-03-14 01:03:17 +0600

    file_upload_service

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -91,6 +91,41 @@ export class Service{
             return false
         }
     }
+
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log(`Appwrite service :: uploadFile :: error${error}`)
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            return await this.storage.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            )
+            return true
+        } catch (error) {
+            console.log(`Appwrite service :: deleteFile :: error${error}`)
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.storage.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service()
